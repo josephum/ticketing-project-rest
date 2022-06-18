0b4353c36146663cd481b8b1047826c4f407d28a
Jose <josephum.garcia@gmail.com>
2022-06-18 12:15:35 -0400

    Creating REST APIs

--- src/main/java/com/cydeo/controller/ProjectController.java
+++ src/main/java/com/cydeo/controller/ProjectController.java
@@ -21,8 +21,8 @@ public class ProjectController {
 
     @GetMapping
     public ResponseEntity<ResponseWrapper> getProjects() {
-        List<ProjectDTO> list = projectService.listAllProjects();
-        return ResponseEntity.ok(new ResponseWrapper("Projects are successfully retrieved",list, HttpStatus.OK));
+        List<ProjectDTO> projects = projectService.listAllProjects();
+        return ResponseEntity.ok(new ResponseWrapper("Projects are successfully retrieved",projects, HttpStatus.OK));
     }
 
     @GetMapping("/{projectCode}")
@@ -37,15 +37,30 @@ public class ProjectController {
         return ResponseEntity.status(HttpStatus.CREATED).body(new ResponseWrapper("Project is successfully created",HttpStatus.CREATED));
 
     }
-//    public ResponseEntity<ResponseWrapper> updateProject() {
-//
-//
-//    }
-//    public ResponseEntity<ResponseWrapper> deleteProject() {
-//
-//
-//    }
 
+    @PutMapping
+    public ResponseEntity<ResponseWrapper> updateProject(@RequestBody ProjectDTO project) {
+        projectService.update(project);
+        return ResponseEntity.ok(new ResponseWrapper("Project is successfully updated",project,HttpStatus.OK));
+    }
+
+    @DeleteMapping("/{projectCode}")
+    public ResponseEntity<ResponseWrapper> deleteProject(@PathVariable("projectCode") String projectCode) {
+        projectService.delete(projectCode);
+        return ResponseEntity.ok(new ResponseWrapper("Project is successfully deleted",HttpStatus.OK));
+    }
+
+    @GetMapping("/manager/project-status")
+    public ResponseEntity<ResponseWrapper> getProjectByManager() {
+        List<ProjectDTO> projects = projectService.listAllProjectDetails();
+        return ResponseEntity.ok(new ResponseWrapper("Projects are successfully retrieved",projects, HttpStatus.OK));
+    }
+
+    @PutMapping("/manager/complete/{projectCode}")
+    public ResponseEntity<ResponseWrapper> managerCompleteProject(@PathVariable("projectCode") String projectCode) {
+        projectService.complete(projectCode);
+        return ResponseEntity.ok(new ResponseWrapper("Project is successfully completed", HttpStatus.OK));
+    }
 
 
 
