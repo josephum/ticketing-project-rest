5b12564afd37cc8c3df695546e882941643e6d23
Jose <josephum.garcia@gmail.com>
2022-06-22 02:35:57 -0400

    Security KeyCloak Auto with creation users

--- src/main/java/com/cydeo/controller/UserController.java
+++ src/main/java/com/cydeo/controller/UserController.java
@@ -1,8 +1,12 @@
 package com.cydeo.controller;
 
+import com.cydeo.annotation.DefaultExceptionMessage;
 import com.cydeo.dto.UserDTO;
 import com.cydeo.entity.ResponseWrapper;
+import com.cydeo.exception.TicketingProjectException;
 import com.cydeo.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -12,6 +16,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/user")
+@Tag(name = "UserController",description = "UserAPI")
 public class UserController {
 
     private final UserService userService;
@@ -23,6 +28,7 @@ public class UserController {
     // CREATE END-POINT showing all users
     @GetMapping
     @RolesAllowed("Admin")
+    @Operation(summary = "Get Users")
     public ResponseEntity<ResponseWrapper> getUsers() {
         List<UserDTO> list = userService.listAllUsers();
         return ResponseEntity.ok(new ResponseWrapper("Users are successfully retrieved",list, HttpStatus.OK));
@@ -30,6 +36,7 @@ public class UserController {
 
     @GetMapping("/{userName}")
     @RolesAllowed("Admin")
+    @Operation(summary = "Get User By Username")
     public ResponseEntity<ResponseWrapper> getUserByUserName(@PathVariable("userName") String userName) {
         UserDTO user = userService.findByUserName(userName);
         return ResponseEntity.ok(new ResponseWrapper("User is successfully retrieved",user,HttpStatus.OK));
@@ -37,6 +44,7 @@ public class UserController {
 
     @PostMapping
     @RolesAllowed("Admin")
+    @Operation(summary = "Create User")
     public ResponseEntity<ResponseWrapper> createUser(@RequestBody UserDTO user) {
         userService.save(user);
         return ResponseEntity.status(HttpStatus.CREATED).body(new ResponseWrapper("User is created successfully",HttpStatus.CREATED));
@@ -44,6 +52,7 @@ public class UserController {
 
     @PutMapping
     @RolesAllowed("Admin")
+    @Operation(summary = "Update User")
     public ResponseEntity<ResponseWrapper> updateUser(@RequestBody UserDTO user) {
         userService.update(user);
         return ResponseEntity.ok(new ResponseWrapper("User is successfully updated",user,HttpStatus.OK));
@@ -51,8 +60,11 @@ public class UserController {
 
     @DeleteMapping("/{userName}")
     @RolesAllowed("Admin")
-    public ResponseEntity<ResponseWrapper> deleteUser(@PathVariable("userName") String userName) {
-        userService.deleteByUserName(userName);
+    @Operation(summary = "Delete User")
+    @DefaultExceptionMessage(defaultMessage = "Failed to delete user")
+    public ResponseEntity<ResponseWrapper> deleteUser(@PathVariable("userName") String userName) throws TicketingProjectException {
+//        userService.deleteByUserName(userName);
+        userService.delete(userName);
 //        return ResponseEntity.ok(new ResponseWrapper("User is successfully deleted",HttpStatus.OK));
         return ResponseEntity.status(HttpStatus.NO_CONTENT).body(new ResponseWrapper("User is deleted successfully",HttpStatus.NO_CONTENT));
     }
--- src/main/java/com/cydeo/entity/ResponseWrapper.java
+++ src/main/java/com/cydeo/entity/ResponseWrapper.java
@@ -1,13 +1,13 @@
 package com.cydeo.entity;
 
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 import org.springframework.http.HttpStatus;
 
 @Getter
 @Setter
 @NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class ResponseWrapper {
 
     private boolean success;
@@ -27,4 +27,6 @@ public class ResponseWrapper {
         this.code = httpStatus.value();
         this.success = true;
     }
+
+
 }
